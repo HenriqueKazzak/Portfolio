c9e6ffedb2d5b8b979e7785fde049a9c76cd7fed
Henrique 'Kazzak' Mendes <henriquekarmierczak@gmail.com>
2022-09-19 16:15:44 -0300

    Adicionado update

--- Spring/aula2/src/main/java/com/kazzak/aula2/controller/EmpregoController.java
+++ Spring/aula2/src/main/java/com/kazzak/aula2/controller/EmpregoController.java
@@ -3,7 +3,6 @@ package com.kazzak.aula2.controller;
 import com.kazzak.aula2.Entity.Emprego;
 import com.kazzak.aula2.EmpregoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 public class EmpregoController {
@@ -38,10 +38,19 @@ public class EmpregoController {
         return "redirect:/";
     }
 
-    @PostMapping(value = "/edit/{id}")
+    @GetMapping(value = "/edit/{id}")
     public String editForm(@PathVariable Long id, Model model){
-        empregoRepository.findById(id);
+        Optional<Emprego> emprego = empregoRepository.findById(id);
+        model.addAttribute("emprego",emprego);
         return "edit";
+    }
+    @PostMapping("/update/{id}")
+    public String updateForm(@PathVariable Long id, @Valid Emprego emprego, BindingResult result, Model model){
+        if (result.hasErrors()) {
+            return "edit";
+        }
+        empregoRepository.save(emprego);
+        return "redirect:/";
     }
 
 }
